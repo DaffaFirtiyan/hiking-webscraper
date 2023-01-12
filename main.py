10c13129dc4862d994da32e17b5902d6dc393e2e
Daffa Firtiyan <mdaffa.anzalna@gmail.com>
2023-01-12 21:32:04 +1300

    wrote output to a text file
    TODO: find a way to ignore the map so it doesn't output

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.wellingtonregionaltrails.com/trails/search?q=&location=&use=&difficulty=13&distance=&type=&start=0&_=1673510890037"
+url = "https://www.wellingtonregionaltrails.com/trails/search?q=&location=&use=&difficulty=14&distance=&type=&start=0&_=1673510890038"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
 
-title = soup.find("title").get_text()
-print(title)
+# write the prettified html to a text file
+with open("output.txt", "w", encoding="utf-8") as file:
+    file.write(soup.prettify())
 
-# print(soup.prettify())
+# extract the title of the web page
+title = soup.find("title").get_text()
+
+# write the title to the text file
+with open("output.txt", "a") as file:
+    file.write(title)
