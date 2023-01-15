5dd6b00641b6ed94baf4518e140b310500a88d5e
Daffa Firtiyan <mdaffa.anzalna@gmail.com>
2023-01-16 09:19:23 +1300

    tutorial completed

--- main.py
+++ main.py
@@ -5,13 +5,6 @@ url = "https://www.wellingtonregionaltrails.com/trails/search?q=&location=&use=&
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
 
-# write the prettified html to a text file
-with open("output.txt", "w", encoding="utf-8") as file:
-    file.write(soup.prettify())
-
-# extract the title of the web page
 title = soup.find("title").get_text()
 
-# write the title to the text file
-with open("output.txt", "a") as file:
-    file.write(title)
+print(title)
--- tutorial.py
+++ tutorial.py
@@ -1,6 +1,43 @@
 import requests
+from bs4 import BeautifulSoup
 
 url = "https://realpython.github.io/fake-jobs/"
 page = requests.get(url)
+soup = BeautifulSoup(page.content, "html.parser")
 
-print(page.text)
+title = soup.find("title").get_text()
+results = soup.find(id="ResultsContainer")
+
+jobElements = results.find_all("div", class_="card-content")
+
+for jobElement in jobElements:
+    title = jobElement.find("h2", class_="title")
+    comp = jobElement.find("h3", class_="company")
+    loc = jobElement.find("p", class_="location")
+    # print(title.text.strip())
+    # print(comp.text.strip())
+    # print(loc.text.strip())
+    # print()
+
+pythonJobs = results.find_all("h2", string=lambda text: "python" in text.lower())
+
+pythonJobsElements = [h2_element.parent.parent.parent for h2_element in pythonJobs]
+
+for jobElement in pythonJobsElements:
+    title = jobElement.find("h2", class_="title")
+    comp = jobElement.find("h3", class_="company")
+    loc = jobElement.find("p", class_="location")
+    print(title.text.strip())
+    print(comp.text.strip())
+    print(loc.text.strip())
+
+    links = jobElement.find_all("a")
+    for link in links:
+        linkURL = jobElement.find_all("a")[1]["href"]
+        print(f"Apply here: {linkURL}\n")
+
+    print()
+
+# print(title)
+# print(jobElement.prettify())
+# print(soup.prettify())
