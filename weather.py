c2acef0796b888d6187a279f4c41d01ec756f3b0
Daffa Firtiyan <mdaffa.anzalna@gmail.com>
2023-01-15 19:51:06 +1300

    Update weather.py

--- weather.py
+++ weather.py
@@ -2,7 +2,7 @@ from requests_html import HTMLSession
 
 session = HTMLSession()
 
-query = "wellington"
+query = input("place: ").capitalize()
 url = f"https://www.google.com/search?q=weather+{query}"
 
 req = session.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.52"})
