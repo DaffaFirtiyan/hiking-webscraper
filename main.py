f8a9de5b4533824b76c31542fb67619c1cd625aa
Daffa Firtiyan <mdaffa.anzalna@gmail.com>
2023-01-16 10:16:08 +1300

    returns a list of hikes and writes it to a csv

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import csv
 
 url = "https://www.wellingtonregionaltrails.com/trails/search?q=&location=&use=&difficulty=14&distance=&type=&start=0&_=1673510890038"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, "html.parser")
 
-title = soup.find("title").get_text()
+driver = webdriver.ChromiumEdge()
+driver.get(url)
+wait = WebDriverWait(driver, 30)
+element = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "el-panel__block")))
+html = driver.page_source
 
-print(title)
+soup = BeautifulSoup(html, "html.parser")
+
+results = soup.find("div", class_=["results", "results--ofsearch"])
+trails = results.find_all("div", class_=["el-panel__block", "el-panel__block--25", "pure-u-16-16", "pure-u-sm-12-24", "pure-u-lg-6-24"])
+
+# print(len(trails))
+with open("trails.csv","w", newline="", encoding="utf-8") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Name", "Location", "Distance"])
+    for trail in trails:
+        name = trail.find("div", class_="el-teaser__title")
+        loc = trail.find("div", class_="info--location").find("span", class_="info__text")
+        dist = trail.find("div", class_="info--distance").find("span", class_="info__text")
+        writer.writerow([name.text.strip(), loc.text.strip(), dist.text.strip()])
+driver.quit()
